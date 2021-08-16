2142cb7adb4363f362ab11ee39c4bd67aba990f9
황성준 <opr99@naver.com>
2021-08-16 23:46:18 +0900

    스프링 시큐리티와 OAuth 2.0 적용

---
+++ src/main/java/com/sungjun/site/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.sungjun.site.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing //JPA Auditing 활성화
+@Configuration
+public class JpaConfig {
+}
--- src/main/java/com/sungjun/site/springboot/web/IndexController.java
+++ src/main/java/com/sungjun/site/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.sungjun.site.springboot.web;
 
+import com.sungjun.site.springboot.config.auth.LoginUser;
+import com.sungjun.site.springboot.config.auth.dto.SessionUser;
 import com.sungjun.site.springboot.domain.posts.PostsRepository;
 import com.sungjun.site.springboot.service.PostsService;
 import com.sungjun.site.springboot.web.dto.PostsResponseDto;
@@ -9,15 +11,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
